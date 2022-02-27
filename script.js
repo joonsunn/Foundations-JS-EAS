5f142b0928a5c7c5df50d25ad72de2e015528c0b
Joon Sunn <joonsunn.foo@gmail.com>
2022-02-27 20:33:26 +0800

    added mouseover and gridsize change prompt

--- script.js
+++ script.js
@@ -4,23 +4,58 @@ container.className = "container";
 container.setAttribute("style", "display: flex; flex-direction: column; align-items:center");
 body.appendChild(container);
 
+const btn = document.createElement("button");
+btn.className = "btn1";
+btn.innerText = "CLEAR GRID";
+container.appendChild(btn);
+
+
+
 let number = 16;
 let rownum = 16;
 let size = 25; //in px
 
-for (let j = 0; j < rownum; j++){
-    const row = document.createElement("div");
-    row.className = "row";
-    row.setAttribute("style", "display: flex; flex-direction:row");
+buildGrid();
 
+btn.addEventListener("click", event =>{
+    document.querySelectorAll(".square").forEach(item => {
+        item.setAttribute("style",`height:${size}px; width:${size}px; border-style: solid; border-width:1px; border-color:grey`)
+    })
+    let gridSize = prompt("Please enter grid size:")
+    
+    number = gridSize;
+    rownum = gridSize;
+    const elements = document.getElementsByClassName("square");
+    while (elements.length > 0) elements[0].remove();
+    buildGrid();
 
-    for (let i = 0; i < number; i++){
-        const square = document.createElement("div");
-        square.className = "square";
-        square.setAttribute("style",`height:${size}px; width:${size}px; border-style: solid; border-width:1px; border-color:grey`);
-        row.appendChild(square);
+})
+
+function buildGrid(){
+    for (let j = 0; j < rownum; j++){
+        const row = document.createElement("div");
+        row.className = "row";
+        row.setAttribute("style", "display: flex; flex-direction:row");
+    
+    
+        for (let i = 0; i < number; i++){
+            const square = document.createElement("div");
+            square.className = "square";
+            square.setAttribute("style",`height:${size}px; width:${size}px; border-style: solid; border-width:1px; border-color:grey`);
+            row.appendChild(square);
+        }
+        container.appendChild(row);
+    
     }
-    container.appendChild(row);
+    
+    document.querySelectorAll(".square").forEach(item => {
+        item.addEventListener("mouseover", event =>{
+            item.setAttribute("style",`height:${size}px; width:${size}px; border-style: solid; border-width:1px; border-color:grey; background-color:red`)
+            
+    
+        })
+    
+    })
 
 }
 
